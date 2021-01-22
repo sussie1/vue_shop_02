bfed9866002e7e8941b9ae24652b59e82f44a52a
sussie <895016978@qq.com>
2021-01-22 15:35:01 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,131 @@
 <template>
-    <div>home 组件
-
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
-
+        <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+        <div>
+        <img src="../assets/heima.png" alt="">
+        <span>后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout"> 退出 </el-button>
+        </el-header>
+         <!-- 页面主体区域 -->
+        <el-container>
+        <!-- 侧边栏 -->
+        <el-aside :width="isCollapse ? '64px': '200px'">
+           <!-- 切换菜单折叠与否按钮 -->
+        <div class="toggle-button" @click='togglecollapse'>|||</div>
+          <!-- 侧边栏菜单区 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened
+        :collapse='isCollapse' :collapse-transition='false' router :default-active='activePath'>
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id" >
+            <!-- 一级菜单的模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="objList[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+             <el-menu-item :index="'/'+subItem.path + ''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/'+subItem.path)">
+              <i class="el-icon-menu"></i>
+              <span slot="title">{{subItem.authName}}</span>
+             </el-menu-item>
+          </el-submenu>
+        </el-menu>
+        </el-aside>
+        <!-- 主体结构 -->
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        </el-container>
+        </el-container>
 </template>
 <script >
 
 export default {
+  data () {
+    return {
+    // 左侧菜单数据
+      menuList: [],
+      objList: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-3702mima',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      // 被激活链接地址
+      activePath: ''
+    }
+  },
+  created () {
+  // 在created阶段请求左侧菜单数据
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      console.log(res.data)
+      this.menuList = res.data
+    },
+    // 点击按钮切换菜单折叠
+    togglecollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 设置菜单项的高亮显示的index
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 <style lang="less" scoped>
-
+  .home-container {
+    height:100%;
+  }
+  .el-header {
+    background-color:#373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    >div {
+      display: flex;
+      align-items: center;
+      >span {
+        padding-left: 10px;
+        color: #fff;
+        font-size: 20px;
+      }
+    }
+  }
+  .el-aside {
+    background-color:#333744;
+  }
+  .el-main{
+  background-color:#eaedf1;
+}
+.el-menu {
+  border-right: 0;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  color: #fff;
+  font-size: 10px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: .2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
@@ -11,9 +11,16 @@ import axios from 'axios'
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 添加axios请求拦截器来添加token，以保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  // 最后必须return
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
